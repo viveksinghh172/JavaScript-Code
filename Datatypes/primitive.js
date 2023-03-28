9b3d4931f79484df7022ea260d649111045f05c0
Vivek <viveksinghh172@gmail.com>
2023-03-29 04:36:11 +0530

    Created new file

---
+++ Datatypes/primitive.js
@@ -0,0 +1,9 @@
+let a = null;
+let b = 345;
+let c = true
+let d = BigInt("567") + BigInt("3")
+let e = "vivek"
+let f = Symbol("i am a nice symbol")
+let g
+document.write(a, b, c, d, e, f, g)
+document.write(typeof c)
